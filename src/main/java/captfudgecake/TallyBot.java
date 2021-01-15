6dc13699fa37d6a5ec32b7c3e62efa9b5db5778e
David Rimmer <rednot1@hotmail.com>
2021-01-15 21:28:11 +0000

    minor refactor

--- src/main/java/captfudgecake/TallyBot.java
+++ src/main/java/captfudgecake/TallyBot.java
@@ -1,23 +1,52 @@
 package captfudgecake;
 
+import java.util.Collection;
+
 import org.javacord.api.*;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.TextChannel;
 /**
  * Hello world!
  *
  */
 public class TallyBot 
 {
+    private DiscordApi api;
+    private TextChannel foodPicsChannel;
+
     public static void main( String[] args )
     {
         String token = "";
-        DiscordApi test = new DiscordApiBuilder()
+        TallyBot bot = new TallyBot(token);
+        bot.addPingPong();
+    }
+
+    TallyBot(String token){
+        this.api = new DiscordApiBuilder()
                                 .setToken(token)
                                 .login().join();
+        
+        getFoodPicsChannel();
+    }
 
-        test.addMessageCreateListener(event -> {
+    private void getFoodPicsChannel(){
+        Collection<Channel> foodPics = api.getChannelsByName("📝discussion");
+        if(foodPics != null && foodPics.size() > 0){
+            foodPicsChannel = foodPics.iterator().next().asTextChannel().get();
+        }
+    }
+
+    public void addPingPong(){
+        api.addMessageCreateListener(event -> {
             if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                System.out.println(event.getChannel().getId());
                 event.getChannel().sendMessage("Pong!");
+                sendMessageToFoodPics("Test message, please ignore");
             }
         });
+    }
+
+    public void sendMessageToFoodPics(String message){
+        foodPicsChannel.sendMessage(message);
     }
 }
